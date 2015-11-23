3407b995d64fc2a7d4c58d91aacbf40ba317c1dd
PCieslewski <Pawel.vip@gmail.com>
2015-11-22 22:49:49 -0500

    Fixed main.

--- src/main/scala-2.11/POJOs/test.scala
+++ src/main/scala-2.11/POJOs/test.scala
@@ -1,16 +1,16 @@
 package POJOs
 
-object test  {
+object test{
 
   def main(args: Array[String]) {
-    val myPro: Profile = new Profile()
+    val myPro: Profile = new Profile("will")
     myPro.birthday = "june"
     myPro.name = "will"
     myPro.status = In_A_Relationship()
 
     println(myPro)
 
-    val will: Friend = new Friend()
+    val will: Friend = new Friend(0,"Pawel")
     will.id = 3
     will.name = "livesey"
 
