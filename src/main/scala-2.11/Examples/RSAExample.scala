0dfd45406b0f23f8d7833199a6b876f7a21d6625
PCieslewski <Pawel.vip@gmail.com>
2015-12-09 18:46:55 -0500

    Working aes example!

---
+++ src/main/scala-2.11/Examples/RSAExample.scala
@@ -0,0 +1,92 @@
+package Examples
+
+import java.io._
+import java.security._
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+import javax.crypto._
+import javax.crypto.spec.SecretKeySpec
+import Examples.crypto.aes
+
+import scala.collection.immutable.Stream
+import scala.Function.const
+
+object RSAExample {
+
+  def main(args: Array[String]) {
+
+    val aeskey = aes.generateSecretKey
+
+    val test: String = "This is a test."
+
+    println(test)
+
+    val testEncrypted = aes.encrypt(aeskey,test.getBytes())
+
+    println(new String(testEncrypted))
+
+    val testDecrypted = aes.decrypt(aeskey,testEncrypted)
+
+    println(new String(testDecrypted))
+
+
+  }
+
+}
+
+object crypto {
+
+  object rsa {
+
+    def encrypt(key: PublicKey, data: Array[Byte]): Array[Byte] = {
+      val cipher = Cipher.getInstance("RSA")
+      cipher.init(Cipher.ENCRYPT_MODE, key)
+      cipher.doFinal(data)
+    }
+
+    def decrypt(key: PrivateKey, data: Array[Byte]): Array[Byte] = {
+      val cipher = Cipher.getInstance("RSA")
+      cipher.init(Cipher.DECRYPT_MODE, key)
+      cipher.doFinal(data)
+    }
+
+    def sign(key: PrivateKey, data: Array[Byte]): Array[Byte] = {
+      val signer = Signature.getInstance("SHA1withRSA")
+      signer.initSign(key)
+      signer.update(data)
+      signer.sign
+    }
+
+    def verify(key: PublicKey, signature: Array[Byte], data: Array[Byte]): Boolean = {
+      val verifier = Signature.getInstance("SHA1withRSA")
+      verifier.initVerify(key)
+      verifier.update(data)
+      verifier.verify(signature)
+    }
+
+  }
+
+  object aes {
+    def encrypt(key: SecretKey, data: Array[Byte]): Array[Byte] = {
+      val cipher = Cipher.getInstance("AES")
+      cipher.init(Cipher.ENCRYPT_MODE, key)
+      cipher.doFinal(data)
+    }
+
+    def decrypt(key: SecretKey, data: Array[Byte]): Array[Byte] = {
+      val cipher = Cipher.getInstance("AES")
+      cipher.init(Cipher.DECRYPT_MODE, key)
+      cipher.doFinal(data)
+    }
+
+    def generateSecretKey: SecretKey = {
+      val generator = KeyGenerator.getInstance("AES")
+      generator.init(128)
+      generator.generateKey
+    }
+
+    def decodeSecretKey(encodedKey: Array[Byte]): SecretKey =
+      new SecretKeySpec(encodedKey, "AES")
+  }
+
+}
